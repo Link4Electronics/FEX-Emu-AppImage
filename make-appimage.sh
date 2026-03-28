5b3d129fe5c53b82722eaa70b9d655d82ae71523
Link4Electronics <link4electronics@gmail.com>
2026-03-28 13:08:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,7 @@ export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/FEX* /usr/share/binfmts/FEX* /var/lib/binfmts/FEX*
+quick-sharun /usr/bin/FEX* /usr/lib/libFEXCore.so /usr/share/fex-emu/* #/usr/share/binfmts/FEX* /var/lib/binfmts/FEX*
 
 # Additional changes can be done in between here
 
